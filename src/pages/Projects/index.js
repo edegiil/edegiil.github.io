502fe4e0e20159cae00764820c90d587f2ea08e8
wingtree <electropark917@gmail.com>
2020-02-17 12:08:53 +0900

    add project link and images

--- src/pages/Projects/index.js
+++ src/pages/Projects/index.js
@@ -1,9 +1,9 @@
 // @flow
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 import React from 'react';
 
 import withLayout from 'hoc/withLayout';
 
-import Layout from 'components/layout';
 import SEO from 'components/seo';
 
 import './styles.scss';
@@ -15,21 +15,31 @@ class Projects extends React.Component {
     window.open(image_url);
   }
 
+  openLink = (url: string) => () => {
+    if (!url) {
+      alert('링크가 없습니다.');
+      return;
+    }
+    window.open(url);
+  }
+
   render() {
-    const { showDetailImage } =  this;
+    const { showDetailImage, openLink } = this;
     const default_image_url = 'https://drive.google.com/uc?id=1EjNE3vC1Z47vpwivH7-L-dyo88o5munv';
+    const m_tree_url = 'https://drive.google.com/uc?id=1vxvaKCv-n_uAXJyCEUIpvkZZh8Twh77n';
+    const busyless_url = 'https://drive.google.com/uc?id=1l6qZX5eQp_Vh6AJyx_aLICKdjNjT5wet';
     return (
       <>
         <SEO title='Projects' />
         <div className='projects-wrapper'>
           <div className='project-wrapper'>
             <div className='project-image-wrapper'>
-              <img className='project-image' alt='project-image' src={default_image_url} onClick={showDetailImage(default_image_url)} />
+              <img className='project-image' alt='project' src={default_image_url} onClick={showDetailImage(default_image_url)} />
             </div>
             <div className='project-content'>
               <div className='project-title'>
                 <div className='project-count'>01.</div>
-                <div className='project-title-text'>플레이픽 - 코디 결정장애 해소</div>
+                <div className='project-title-text' onClick={openLink()}>플레이픽 - 코디 결정장애 해소</div>
               </div>
               <div className='project-content-wrapper'>
                 <div className='project-content-title-wrapper'>
@@ -48,16 +58,16 @@ class Projects extends React.Component {
                 </div>
               </div>
             </div>
-          </div>  
+          </div>
           <div className='project-divider' />
           <div className='project-wrapper'>
             <div className='project-image-wrapper'>
-              <img className='project-image' alt='project-image' src={default_image_url} onClick={showDetailImage(default_image_url)} />
+              <img className='project-image' alt='project' src={m_tree_url} onClick={showDetailImage(m_tree_url)} />
             </div>
             <div className='project-content'>
               <div className='project-title'>
                 <div className='project-count'>02.</div>
-                <div className='project-title-text'>M-TREE 카페 홈페이지</div>
+                <div className='project-title-text' onClick={openLink('https://m-tree.club/')}>M-TREE 카페 홈페이지</div>
               </div>
               <div className='project-content-wrapper'>
                 <div className='project-content-title-wrapper'>
@@ -76,16 +86,16 @@ class Projects extends React.Component {
                 </div>
               </div>
             </div>
-          </div>  
+          </div>
           <div className='project-divider' />
           <div className='project-wrapper'>
             <div className='project-image-wrapper'>
-              <img className='project-image' alt='project-image' src={default_image_url} onClick={showDetailImage(default_image_url)} />
+              <img className='project-image' alt='project' src={busyless_url} onClick={showDetailImage(busyless_url)} />
             </div>
             <div className='project-content'>
               <div className='project-title'>
                 <div className='project-count'>03.</div>
-                <div className='project-title-text'>비지리스 - 내게 필요한 창업지원사업만 모아보세요</div>
+                <div className='project-title-text' onClick={openLink('https://play.google.com/store/apps/details?id=com.ailtos.busyless')}>비지리스 - 내게 필요한 창업지원사업만 모아보세요</div>
               </div>
               <div className='project-content-wrapper'>
                 <div className='project-content-title-wrapper'>
@@ -104,11 +114,11 @@ class Projects extends React.Component {
                 </div>
               </div>
             </div>
-          </div>  
+          </div>
         </div>
       </>
-    )
+    );
   }
 }
 
-export default withLayout(Projects)({ title: 'Projects', width: 'full' })
+export default withLayout(Projects)({ title: 'Projects', width: 'full' });
