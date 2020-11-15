a62b680fa8c1f8f8bf4f9321be33e44907900dee
wingtree <electropark917@gmail.com>
2020-11-15 14:18:51 +0900

    Feature: add helmet

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,11 +6,24 @@
 
 module.exports = {
   siteMetadata: {
-    title: 'Wingtree Devlog',
+    title: 'wingtree.github.io',
     siteUrl: 'https://wingtree.github.io',
     description: 'wingtree의 개발자 블로그입니다.',
   },
   plugins: [
     'gatsby-plugin-styled-components',
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'wingtree.github.io',
+        short_name: 'wingtree',
+        start_url: '/',
+        background_color: '#16171b',
+        theme_color: '#16171b',
+        display: 'standalone',
+        icon: 'src/assets/favicon.png',
+      },
+    },
+    'gatsby-plugin-react-helmet',
   ],
 };
