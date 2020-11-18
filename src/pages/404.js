65c81a505ac2e3345d1ec5044185e851f5865ea9
wingtree <electropark917@gmail.com>
2020-11-18 23:49:22 +0900

    Feature: add 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,8 +1,33 @@
 import React from 'react';
+import styled from 'styled-components';
+import {Link} from 'gatsby';
+
+const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.h1`
+  font-weight: bold;
+  margin: 0;
+`;
+
+const Subtitle = styled.h4`
+  font-weight: 100;
+  margin: 0;
+  margin-bottom: 20px;
+`;
 
 function _404() {
   return (
-    <div>404</div>
+    <Container>
+      <Title>404</Title>
+      <Subtitle>공허...</Subtitle>
+      <Link to='/'>입구로 돌아가기</Link>
+    </Container>
   );
 }
 
