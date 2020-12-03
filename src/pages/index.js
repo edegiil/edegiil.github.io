a835ae8300ed2283e0c4469ce1086f85c94e5097
edegiil <electropark917@gmail.com>
2020-12-03 15:54:00 +0900

    Update: add hovering event at main page title

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useCallback} from 'react';
 import styled from 'styled-components';
 import {graphql} from 'gatsby';
 import BackgroundImage from 'gatsby-background-image';
@@ -22,23 +22,42 @@ const TitleGroup = styled.div`
 `;
 
 const Title = styled.h1`
+  display: flex;
+  align-items: center;
   font-weight: 100;
+  width: 10rem;
+  height: 4rem;
   margin: 0;
-  font-family: 'Sindarin', sans-serif;
+  font-family: ${(props) => {
+    const {isSindarin} = props;
+    return isSindarin ? `'Sindarin', sans-serif` : undefined;
+  }};
 `;
 
 const SubTitle = styled.div`
 `;
 
 function Home(props) {
+  const [title_hover_state, setTitleHoverState] = useState(false);
+
   const {data} = props;
   const background_image = data.file.childImageSharp.fluid;
+
+  const handleTitleHover = useCallback(() => {
+    setTitleHoverState((s) => !s);
+  }, []);
 
   return (
     <BackgroundImage fluid={background_image}>
       <Background>
         <TitleGroup>
-          <Title>3G2E7G5</Title>
+          <Title
+            onMouseEnter={handleTitleHover}
+            onMouseOut={handleTitleHover}
+            isSindarin={!title_hover_state}
+          >
+            {title_hover_state ? 'EDEGIL' : '3G2E7G5'}
+          </Title>
           <SubTitle>seven stars</SubTitle>
         </TitleGroup>
       </Background>
