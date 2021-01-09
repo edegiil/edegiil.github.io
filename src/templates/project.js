b6d620a915250ca921ae1770d1174f917f9937ca
edegiil <electropark917@gmail.com>
2021-01-09 23:26:37 +0900

    Update: apply project template page

--- src/templates/project.js
+++ src/templates/project.js
@@ -1,6 +1,9 @@
-import React, {useEffect} from 'react';
+import React, {useCallback, useEffect} from 'react';
 import styled from 'styled-components';
-import {graphql} from 'gatsby';
+import {graphql, Link} from 'gatsby';
+
+import back_icon from 'assets/icons/back.svg';
+import link_icon from 'assets/icons/link.svg';
 
 const Background = styled.div`
   display: flex;
@@ -11,9 +14,25 @@ const Background = styled.div`
   align-items: center;
 `;
 
-const Nav = styled.div`
+const ContentBox = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
   width: 900px;
-  margin-bottom: 4px;
+`;
+
+const NavGroup = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin-bottom: 16px;
+`;
+
+const NavText = styled.div``;
+
+const Icon = styled.img`
+  width: 30px;
+  height: 30px;
 `;
 
 const TitleGroup = styled.div`
@@ -25,9 +44,12 @@ const TitleGroup = styled.div`
 
 const Title = styled.h2`
   margin: 0;
+  margin-right: 8px;
 `;
 
-const Link = styled.img``;
+const ProjectLink = styled.img`
+  cursor: pointer;
+`;
 
 const InfoGroup = styled.div`
   width: 900px;
@@ -42,28 +64,44 @@ const InfoText = styled.div`
   margin: 0;
 `;
 
-const ContentWrapper = styled.main``;
+const ContentWrapper = styled.main`
+  width: 900px;
+`;
 
 function ProjectTemplate({data}) {
   const {html, frontmatter} = data.markdownRemark;
-  const {title, summary, time, platform} = frontmatter;
+  const {title, summary, time, platform, link} = frontmatter;
 
   useEffect(() => {
-    document.getElementById('content').innerHTML = html;
+    document.getElementById('content-project').innerHTML = html;
   });
+
+  const handleLink = useCallback(() => {
+    window.open(link);
+  }, [link]);
 
   return (
     <Background>
-      <Nav>돌아가기</Nav>
-      <TitleGroup>
-        <Title>{title}</Title>
-        <Link />
-      </TitleGroup>
-      <InfoGroup>
-        <Subtitle>{summary}</Subtitle>
-        <InfoText>{`${platform} | ${time}`}</InfoText>
-      </InfoGroup>
-      <ContentWrapper id='content' />
+      <ContentBox>
+        <Link to='/project' replace>
+          <NavGroup>
+            <Icon src={back_icon} />
+            <NavText>돌아가기</NavText>
+          </NavGroup>
+        </Link>
+        <TitleGroup>
+          <Title>{title}</Title>
+          {
+            link &&
+            <ProjectLink src={link_icon} onClick={handleLink} />
+          }
+        </TitleGroup>
+        <InfoGroup>
+          <Subtitle>{summary}</Subtitle>
+          <InfoText>{`${platform} | ${time}`}</InfoText>
+        </InfoGroup>
+        <ContentWrapper id='content-project' />
+      </ContentBox>
     </Background>
   );
 }
@@ -77,6 +115,7 @@ export const query = graphql`
         summary
         time
         platform
+        link
       }
     }
   }
