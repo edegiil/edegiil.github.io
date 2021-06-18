0519ccd8ccd135562e4cef434356e0936c4d41c8
edegiil <electropark917@gmail.com>
2021-06-18 23:56:45 +0900

    Update: Devlog 리스트 페이지 반응형 적용

--- src/templates/devlogList.js
+++ src/templates/devlogList.js
@@ -20,6 +20,16 @@ const Main = styled.main`
   flex-direction: column;
   align-items: center;
   align-content: start;
+  justify-items: center;
+  @media ${(props) => props.theme.tablet} {
+    width: 80%;
+    row-gap: 24px;
+  }
+  @media ${(props) => props.theme.mobile} {
+    width: 90%;
+    row-gap: 16px;
+    padding-bottom: 60px;
+  }
 `;
 
 const TitleGroup = styled.div`
@@ -28,10 +38,9 @@ const TitleGroup = styled.div`
   width: 800px;
   @media ${(props) => props.theme.tablet} {
     width: 100%;
-    padding: 0 5%;
   }
   @media ${(props) => props.theme.mobile} {
-    padding: 0 5%;
+    width: 100%;
   }
 `;
 
@@ -49,10 +58,14 @@ const Content = styled.div`
   grid-template-columns: repeat(3, 1fr);
   width: 800px;
   @media ${(props) => props.theme.tablet} {
-    width: 45%;
+    width: 530px;
+    grid-template-columns: repeat(2, 1fr);
+    justify-items: center;
   }
   @media ${(props) => props.theme.mobile} {
     width: 100%;
+    grid-template-columns: repeat(1, 1fr);
+    justify-items: center;
   }
 `;
 
